b391c7ee56e843f9059c26801ff5f5e903fa25b4
cintick <drummercintick@gmail.com>
2023-08-08 23:56:36 +0300

    8agustosH

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,6 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float SpeedMultiplier;
-    public GameObject playerObject;
 
     // Start is called before the first frame update
     void Start()
@@ -18,23 +17,29 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput   = Input.GetAxis("Horizontal");
-        float verticalInput     = Input.GetAxis("Vertical");
+        CalculateMovement();
+    }
 
-        if (transform.position.x > 9)
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // wrap player around in X axis. ( exiting from left of the screen and teleporting right )
+        if (transform.position.x > 11.35)
         {
-            transform.position = new Vector3(9, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-11.3f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -11.35)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, transform.position.z);
         }
 
-        if (transform.position.x < -9)
-        {
-            transform.position = new Vector3(-9, transform.position.y, transform.position.z);
+        // restrict player to only moving between zero and bottom of the screen with Mathf.Clamp
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.85f, 0), 0);
 
-        }
-
-
+        //move player with the speed multipler in horizontal and vertical axises
         transform.Translate(new Vector3(1, 0, 0) * horizontalInput * Time.deltaTime * SpeedMultiplier);
         transform.Translate(new Vector3(0, 1, 0) * verticalInput * Time.deltaTime * SpeedMultiplier);
-
     }
 }
